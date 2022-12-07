324b4158ce7aae9a8836435ad03ab2c0fbc7e3b4
AleksandrVavilov <vavilov3388@gmail.com>
2022-12-07 12:36:57 +0300

    Two Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        String str = "wssw";
+        System.out.println(readTwo(str));
     }
+    public static boolean readTwo (String args){
+        return args.equals(new StringBuilder(args).reverse().toString());
+    }
+
 }
